9261c1baad76b0150cdd08fd4cb7d3d5963a7ee6
Anmoltuli1 <anmoltuli36@gmail.com>
2021-12-23 21:20:11 +0530

    order,product and user router are implemented

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 
@@ -20,6 +23,9 @@ app.get("/api/test", ()=>{
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("server is running on port: 5000")
